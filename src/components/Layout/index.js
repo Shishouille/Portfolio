ea53250ad8265201f2a51a93013dae9c0f74c6b1
Shishouille <shirin.boomi@zoho.eu>
2020-04-28 14:02:34 +0200

    :mag:  Fixed Headers

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navigation from './Navigation';
 import Footer from './Footer';
 import StyledLayout from './StyledLayout';
 
 const Layout = ({ children }) => (
   <StyledLayout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Shirin Boomi - Front-end Developper</title>
+      <meta name="portolio" content="This is my portfolio." />
+      <link rel="canonical" href="http://shirin-boomi.netlify.app" />
+    </Helmet>
     <Navigation />
     {children}
     <Footer />
